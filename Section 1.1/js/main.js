33a5c6bbf00fce22cfb52d9f43291b044c3b67de
Christina <christina.brgs@gmail.com>
2023-03-11 21:23:39 -0500

    jsway practice

---
+++ Section 1.1/js/main.js
@@ -0,0 +1,12 @@
+//  name and age 
+
+console.log ('Christina')
+console.log('26')
+
+
+// results of adding, subtracting, multiplying and dividing 6 by 3
+
+console.log(6 + 3)
+console.log (6 - 3)
+console.log (6 * 3)
+console.log(6 / 3)
